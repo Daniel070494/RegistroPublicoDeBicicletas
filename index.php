cb961229ed93880d50fc29342c31a218d60f27cc
Daniel Olguín Ciplina <d.olguin07@outlook.es>
2022-11-24 21:41:31 -0600

    Se creó el proyecto.

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+       echo '<h1>Inicio del proyecto</h1>';
+        ?>
+    </body>
+</html>
